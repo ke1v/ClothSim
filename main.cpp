2e6cabc79bf5c29ee6f4fef5aa65104a330240f7
Kelvin <kelvinw138@gmail.com>
2022-06-27 22:06:06 -0700

    working mesh generator. see main.cpp for input parameters

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <iostream>
+#include <vector>
 
 #include "Point.hpp"
 #include "SFML/Window/Keyboard.hpp"
@@ -12,21 +13,39 @@
 const int WINDOW_SIZE = 1000;
 
 int main(int, char**) {
+	int width, height, wind, pin;
+	std::cin >> width;
+	std::cin >> height;
+	std::cin >> wind;
+	std::cin >> pin;
+	int spacing = 800/width;
 	sf::RenderWindow rwindow(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE), "Verlet Cloth Simulator");
 	sf::Clock clock;
-	Point point1(sf::Vector2f(200.f, 200.f));
-	point1.changeState(Point::State::Pinned);
-	Point point2(sf::Vector2f(100.f, 200.f));
-	// point2.changeState(Point::State::Pinned);
-	Point point3(sf::Vector2f(150.f, 250.f));
 
-	Stick stick1(point1, point3, 100.f);
-	Stick stick2(point2, point3, 100.f);
+	std::vector<Point> points;
+	std::vector<Stick> sticks;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			// points.push_back(Point({i*100.f+150.f, j*100.f+100.f}, {i*100.f+140.f, j*100.f+90.f}));
+			points.push_back(Point({i*spacing+100.f, j*spacing+100.f}));
+			if (j == 0 && i % pin == 0) points[i*height+j].changeState(Point::State::Pinned);
+		}
+	}
+
+	for (int i = 0; i < points.size()-1; i++) {
+		if (i % width != width-1) {
+			sticks.push_back(Stick(points[i], points[i+1], spacing));
+		}
+		if (i + width < points.size()) {
+			sticks.push_back(Stick(points[i], points[i+width], spacing));
+		}
+	}
 
 	bool start = false;
 
-	while (rwindow.isOpen() && clock.getElapsedTime().asSeconds() < 0.01667f) {
+	while (rwindow.isOpen()) {
 		// 60 tps lock
+		if (clock.getElapsedTime().asSeconds() < 0.01667f) continue;
 		sf::Time delta = clock.restart();
 
 		sf::Event event;
@@ -35,14 +54,9 @@ int main(int, char**) {
 				rwindow.close();
 			}
 			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::V) {
-					point1.DisplaySpeed();
-				}
 				if (event.key.code == sf::Keyboard::U) {
 					start = true;
-				}
-				if (event.key.code == sf::Keyboard::D) {
-					stick1.Debug();
+					std::cout << "Start" << std::endl;
 				}
 			}
 		}
@@ -50,20 +64,22 @@ int main(int, char**) {
 		rwindow.clear();
 
 		if (start) {
-			point1.update(delta.asSeconds(), {0.f, 100.f});
-			point2.update(delta.asSeconds(), {0.f, 100.f});
-			point3.update(delta.asSeconds(), {0.f, 100.f});
+			for (auto& p : points) {
+				p.update(delta.asSeconds(), {(float)wind, 100.f});
+			}
 
-			stick1.constrainPoints();
-			stick2.constrainPoints();
+			for (auto& s : sticks) {
+				s.constrainPoints();
+			}
 		}
 
-		// std::cout << "(" << point1.getPosition().x << ", " << point1.getPosition().y << ")" << std::endl;
-		point1.draw(rwindow);
-		point2.draw(rwindow);
-		point3.draw(rwindow);
-		stick1.draw(rwindow);
-		stick2.draw(rwindow);
+		for (auto& s : sticks) {
+			s.draw(rwindow);
+		}
+		for (auto& p : points) {
+			p.draw(rwindow);
+		}
+
 		rwindow.display();
 	}
 
